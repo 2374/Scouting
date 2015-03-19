428593a240091659a3ba9e5da38c1066f048f479
2374 <crusaderbots@gmail.com>
2015-03-19 16:48:36 -0700

    Add Url

--- teams.js
+++ teams.js
@@ -1,4 +1,5 @@
 request = new XMLHttpRequest();
+var url="www.thebluealliance.com";
 var path="/api/v2/event/2015orphi";
 var param="?X-TBA-App-Id=frc2374:scouting:0.01";
 request.open("GET", url+path+param , false);
